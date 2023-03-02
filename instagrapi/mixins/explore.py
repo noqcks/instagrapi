92b41933f871e1e398d3cb8173fc156349ed258c
Benji Visser <benji@093b.org>
2023-03-02 08:30:52 -0700

    report explore media
    Signed-off-by: Benji Visser <benji@093b.org>

--- instagrapi/mixins/explore.py
+++ instagrapi/mixins/explore.py
@@ -4,3 +4,10 @@ class ExploreMixin:
     def get_explore_page(self):
         result = self.private_request("discover/topical_explore")
         return result
+
+    def report_explore_media(self, media_pk: str):
+        params = {
+            'm_pk': media_pk,
+        }
+        result = self.private_request("discover/explore_report/", params=params)
+        return result['explore_report_status'] == "OK"
